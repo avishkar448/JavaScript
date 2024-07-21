67674cf04ec51c554e88805654f40d5b4980632b
Avishkar Gawali <119069300+avishkar448@users.noreply.github.com>
2024-07-21 22:13:31 +0530

    clouser

---
+++ JavascriptQuestion/clousers.js
@@ -0,0 +1,53 @@
+//lexical scope
+function outerScope() {
+  let name = "Deadpool";
+  function innerScope() {
+    let x = "Deadpool two ";
+    console.log("inner ", name);
+  }
+  console.log()
+  function innerTwo() {
+    // console.log(x); //  cannot access the value of another function
+    console.log(name); // access from outer function
+  }
+  innerScope();
+  innerTwo();
+}
+outerScope();
+
+//clouser
+function makeFun() {
+  const name = "Avishkar";
+  function displayName() {
+    console.log(name);
+  }
+  return displayName; // whole lexical scope return
+}
+
+const myFun = makeFun();
+myFun();
+
+//example of clouser
+function createCounter() {
+    let count = 0;
+
+    return {
+        increment: function() {
+            count++;
+            return count;
+        },
+        decrement: function() {
+            count--;
+            return count;
+        },
+        getCount: function() {
+            return count;
+        }
+    };
+}
+
+const counter = createCounter();
+console.log(counter.increment()); // 1
+console.log(counter.increment()); // 2
+console.log(counter.decrement()); // 1
+console.log(counter.getCount());  // 1
